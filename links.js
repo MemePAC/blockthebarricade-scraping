afe90e56f0a8d44d4440ca6c824123662ff7f3e0
Jackie Ni <jackieni2002@gmail.com>
2024-05-15 20:51:04 -0700

    restored package.json

--- links.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import fs from "fs/promises";
-
-const source = "rows.json";
-const file = await fs.readFile(source, "utf-8");
-const list = JSON.parse(file);
-
-const titles = list.map((item) => item.title);
-const unique = [...new Set(titles)];
